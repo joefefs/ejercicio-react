43c4594d1dd9a769a7fb7124b3bbcde875c2b9ab
josefefs <design.joseferrer@gmail.com>
2022-02-19 00:39:26 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import payloadOrder from './payloadOrder';
+import paymentPayload1 from './paymentPayload1';
 import './App.css';
 
 function App() {
+ 
+  const [respuesta, setRespuesta] = useState()
+  const [pasarPago, setPasarPago] = useState()
+ 
+  const handleClick = ()=> {
+  const place = axios.post('https://giftcardsapidev.azurewebsites.net/api/orders', payloadOrder)
+      .then((res) => {
+        setRespuesta(res.data.result)
+        console.log(res.data.result)
+      })
+      // .catch((err) => {
+      //   setRespuesta(err.data.result)
+      //   console.log(err)
+      // })
+  }
+  
+  // const handlePayment = () => {
+  //   const place2 = axios.post('https://giftcardsapidev.azurewebsites.net/api/payment', JSON.parse(paymentPayload1.orderId))
+  //   .then((res) => {
+  //     setPasarPago(res)
+  //     console.log(JSON.stringify(pasarPago.orderId) + " clicked")
+  //   })
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={handleClick}> Crear orden </button>
+      <br />
+      {JSON.stringify(respuesta)}
+      
+      {respuesta === 'Success' ? (<div><br /><button >Pago</button></div>) : null}
+  
       </header>
     </div>
   );
---
+++ src/paymentPayload1.js
@@ -0,0 +1,16 @@
+const paymentPayload1 = {
+    "orderId":"82a05f69-9d74-4d78-acf5-229cd64c8d59",
+    "card":
+    {
+        "number":"4000000000003220",
+        "expMonth":9,
+        "expYear":2022,
+        "Cvv":"031"
+    },
+    "user":{
+        "name":"Manuel Valdes",
+        "email":"valmans@hotmail.com"
+    },
+    "saveMethod":false
+ }
+export default paymentPayload1 
