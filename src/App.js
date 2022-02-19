640619e19394e0ddebe12061f16805246d6f7400
josefefs <design.joseferrer@gmail.com>
2022-02-19 15:11:04 -0600

    added react router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React, {useState, useEffect} from 'react'
 import payloadOrder from './payloadOrder';
 import paymentPayload1 from './paymentPayload1';
 import './App.css';
+import {BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
  
   const [respuesta, setRespuesta] = useState()
   const [pago, setPago] = useState(paymentPayload1)
-  const [url, setUrl] = useState()
-  const [paso, setPaso] = useState(false)
+  const [url, setUrl] = useState({
+    url: "",
+    status: "",
+    hasSucceed: null,
+    error: ""
+  })
  
   const handleClick = ()=> {
   axios.post('https://giftcardsapidev.azurewebsites.net/api/orders', payloadOrder)
@@ -18,43 +23,69 @@ function App() {
         console.log(res.data.result)
        setPago(prevPago =>({
          ...prevPago,
-         orderId: res.data.object.id
+         orderId: '2'//res.data.object.id
 
        }))
         console.log(res.data.object.id)
       })
      
-      // .catch((err) => {
-      //   setRespuesta(err.data.result)
-      //   console.log(err)
-      // })
+      .catch(() => {
+        setPago(prevPago => ({
+          ...prevPago,
+          error: 'Hubo un error: 400 - Bad request.'
+        }))
+       })
   }
   
   const handlePayment = () => {
-   setPaso(!paso)
-    axios.post('https://giftcardsapidev.azurewebsites.net/api/payment', (pago))
-    .then((res) => {
-      console.log(res.data.url)
-      setUrl(res.data.url)
-      console.log(url)
+      axios.post('https://giftcardsapidev.azurewebsites.net/api/payment', pago)
+      .then((res) => {
+        setUrl(prevUrl => {
+          if(res.data.url){ 
+             return ({
+               ...prevUrl,
+                url: res.data.url,
+                status: res.status,
+                hasSucceed: true
+              })
+      } 
+      console.log(prevUrl)
+      })
+      
+    })
+    .catch((err) =>{
+      setUrl(prevUrl => {
+        return ({
+          ...prevUrl,
+          hasSucceed: false,
+          error: "Hubo un error: 400 - Bad request."
+        })
+      })
+      console.log(err)
+      
+    })
+  }
+
+    let hadSuccess
+    if(url.hasSucceed === true) {
+          hadSuccess = (<iframe src={url.url} width="600px" height="500px"></iframe>)
+        } else if (url.error) {
+          hadSuccess = (
+
+          <p>{url.error}</p>
+          )
         }
-      )
-      
-      
-    }
-  
 
   return (
     <div className="App">
       <header className="App-header">
       <button onClick={handleClick}> Crear orden </button>
       <br />
-      {JSON.stringify(respuesta)}
+      {pago.error ? (<p>{pago.error}</p>): null }
       
       {respuesta === 'Success' ? (<div><br /><button onClick={handlePayment} >Pago</button></div>) : null}
       <br />
-      {paso === true && <iframe src={url} width="600px" height="500px"></iframe>}
-  
+      {hadSuccess}  
       </header>
     </div>
   );
